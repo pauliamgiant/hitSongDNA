323a99c06dbf12305f7ede84040952a52964983e
pauliamgiant <pauliamgiant@gmail.com>
2020-05-31 21:17:33 +0100

    Small addition to Classifier class. Sketching initial methods.

--- src/classifier/Classifier.java
+++ src/classifier/Classifier.java
@@ -2,9 +2,19 @@ package classifier;
 
 public class Classifier {
 
+    /**
+     *
+     * @return A Naive bayes classifier object
+     *
+     */
+
     public String testing(){
 
         return "This class is integrating fine";
+    }
+
+    public double calculateClassPriorProbability(){
+        return 0.01;
     }
 
     public boolean songIsLikelyToBeAHit(){
