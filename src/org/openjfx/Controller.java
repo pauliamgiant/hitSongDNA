097601d20954e4143d0bc977b0056e809d037746
pauliamgiant <pauliamgiant@gmail.com>
2020-06-03 10:39:40 +0100

    Some basic positioning of App elements on UI template

--- src/org/openjfx/Controller.java
+++ src/org/openjfx/Controller.java
@@ -1,15 +1,22 @@
 package org.openjfx;
 
 import classifier.Classifier;
+import com.dlsc.formsfx.model.structure.Field;
+import com.dlsc.formsfx.model.structure.Form;
+import com.dlsc.formsfx.model.structure.Group;
+import com.dlsc.formsfx.view.renderer.FormRenderer;
 import eu.hansolo.medusa.Gauge;
-import eu.hansolo.medusa.GaugeBuilder;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.VBox;
 
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Controller {
@@ -21,42 +28,68 @@ public class Controller {
     @FXML
     private Gauge gauge1;
 
+    @FXML
+    private VBox forForm;
 
     @FXML
     private Button catchMe;
 
     @FXML
-    public void initialize() throws URISyntaxException {
-       Image image = new Image(getClass().getResource("Resources/1.jpg").toURI().toString());
+    private VBox myPie;
 
-        gauge1 = GaugeBuilder.create()
-                .skinType(Gauge.SkinType.AMP)
-                .titleColor(Color.WHITE)
-                .ledVisible(true)
-                .backgroundPaint(Color.WHITE)
-                .foregroundPaint(Color.BLACK)
-                .lcdVisible(true)
-                .shadowsEnabled(true)
-                .animated(true)
-                .animationDuration(500)
-                .title("title")                                                                  // Text for title
-                .titleColor(Color.WHITE)
-                .ledVisible(true)                                                               // Should LED be visible
-                .ledType(Gauge.LedType.STANDARD)                                                       // Type of the LED (STANDARD, FLAT)
-                .ledColor(Color.rgb(99, 200, 0))                                                // Color of LED
-                .ledBlinking(true)                                                              // Should LED blink
-                .ledOn(true)   // Color for title text
-                .unit("unit")                                                                    // Text for unit
-                .unitColor(Color.BLACK)
-                .build();
+    @FXML
+    public void initialize() throws URISyntaxException {
+        // Image image = new Image(getClass().getResource("Resources/1.jpg").toURI().toString());
+
+        List<String> tempos = new ArrayList<>();
+        tempos.add("below 80");
+        tempos.add("80-100");
+        tempos.add("100-110");
+        tempos.add("110-120");
+        tempos.add("120-130");
+        tempos.add("above 130");
+
+        Form loginForm = Form.of(
+                Group.of(
+                        Field.ofStringType("")
+                                .label("Song Title")
+                                .required("This field can’t be empty"),
+                        Field.ofStringType("")
+                                .label("Duration")
+                                .required("This field can’t be empty"),
+                        Field.ofMultiSelectionType(tempos)
+                                .label("Tempo Range")
+                                .required("This field can’t be empty"),
+                        Field.ofSingleSelectionType(tempos)
+                                .label("Key")
+                                .required("This field can’t be empty"),
+                        Field.ofSingleSelectionType(tempos)
+                                .label("Mode")
+                                .required("This field can’t be empty")
+
+                ),
+                Group.of()
+
+        ).title("Login");
+        forForm.getChildren().add(new FormRenderer(loginForm));
+
 
         Classifier paulsCl = new Classifier();
         catchMe.setText(paulsCl.testing());
         System.out.println(paulsCl.testing());
-       imView.setImage(image);
+        //imView.setImage(image);
+        ObservableList<PieChart.Data> myPieG = FXCollections.observableArrayList(
+                new PieChart.Data("Grapefruit", 13),
+                new PieChart.Data("Oranges", 25),
+                new PieChart.Data("Plums", 10),
+                new PieChart.Data("Pears", 22),
+                new PieChart.Data("Apples", 30));
+        final PieChart chart = new PieChart(myPieG);
+        chart.setMaxSize(800, 800);
+        myPie.getChildren().add(chart);
+
+        chart.setTitle("Imported Fruits");
     }
-
-
 
 
 }
--- src/org/openjfx/HitSongDNA.java
+++ src/org/openjfx/HitSongDNA.java
@@ -1,23 +1,9 @@
 package org.openjfx;
 
-import com.dlsc.formsfx.model.structure.Field;
-import com.dlsc.formsfx.model.structure.Form;
-import com.dlsc.formsfx.model.structure.Group;
-import com.dlsc.formsfx.view.renderer.FormRenderer;
-import eu.hansolo.medusa.Gauge;
-import eu.hansolo.medusa.GaugeBuilder;
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.chart.PieChart;
-import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 
@@ -25,99 +11,11 @@ public class HitSongDNA extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //Parent root = FXMLLoader.load(getClass().getResource("UI.fxml"));
-        MenuBar menuBar = new MenuBar();
-        Menu fileMenu = new Menu("File");
-        fileMenu.getItems().add(new MenuItem("Save"));
-        menuBar.getMenus().add(fileMenu);
+        Parent root = FXMLLoader.load(getClass().getResource("UI.fxml"));
 
-
-
-        Button okButton = new Button("Ok");
-        okButton.setDefaultButton(true);
-
-        Button cancelButton = new Button("Cancel");
-
-        Button icon = new Button("ICON HERE");
-
-        VBox vBox1 = new VBox();
-        vBox1.setPadding(new Insets(15, 12, 15, 12));
-        vBox1.setSpacing(10);
-        vBox1.setStyle("-fx-background-color: deeppink");
-        vBox1.getChildren().addAll(okButton, cancelButton, icon);
-
-        BorderPane root = new BorderPane();
-        root.setTop(menuBar);
-        root.setRight(vBox1);
-
-        Image image = new Image(getClass().getResource("Resources/1.jpg").toURI().toString());
-        ImageView imageView = new ImageView(image);
-
-
-        System.out.println(System.getProperty("user.dir"));
-
-        ObservableList<PieChart.Data> pieChartData =
-                FXCollections.observableArrayList(
-                        new PieChart.Data("Grapefruit", 13),
-                        new PieChart.Data("Oranges", 25),
-                        new PieChart.Data("Plums", 10),
-                        new PieChart.Data("Pears", 22),
-                        new PieChart.Data("Apples", 30));
-        final PieChart chart = new PieChart(pieChartData);
-
-        chart.setTitle("Imported Fruits");
-        //root.setCenter(chart);
-        Form loginForm = Form.of(
-                Group.of(
-                        Field.ofStringType("")
-                                .label("Username"),
-                        Field.ofStringType("")
-                                .label("Password")
-                                .required("This field can’t be empty")
-                ),
-                Group.of()
-        ).title("Login");
-
-
-        Gauge gauge2 = GaugeBuilder.create()
-                .skinType(Gauge.SkinType.AMP)
-
-                .titleColor(Color.WHITE)
-                .ledVisible(true)
-                .backgroundPaint(Color.WHITE)
-                .foregroundPaint(Color.BLACK)
-                .lcdVisible(true)
-                .shadowsEnabled(true)
-                .animated(true)
-                .animationDuration(500)
-                .title("Title")                                                                  // Text for title
-                .titleColor(Color.WHITE)
-                .ledVisible(true)                                                               // Should LED be visible
-                .ledType(Gauge.LedType.STANDARD)                                                       // Type of the LED (STANDARD, FLAT)
-                .ledColor(Color.rgb(99, 200, 0))                                                // Color of LED
-                .ledBlinking(true)                                                              // Should LED blink
-                .ledOn(true)   // Color for title text
-                .unit("unit")                                                                    // Text for unit
-                .unitColor(Color.BLACK)
-                .build();
-
-
-        root.setCenter(gauge2);
-        root.setBottom(new FormRenderer(loginForm));
-
-
-        VBox vBox = new VBox();
-        vBox.setStyle("-fx-background-color: #ddeeff; -fx-padding: 10px");
-
-        vBox.getChildren().addAll(new Label("Select Color:"), new ColorPicker());
-        root.setLeft(vBox);
-
-
-
-        Scene scene = new Scene(root, 740, 800);
-        primaryStage.setTitle("Now we're talking");
+        Scene scene = new Scene(root, 1280, 768);
+        primaryStage.setTitle("HitSongDNA");
         primaryStage.setScene(scene);
-
         primaryStage.show();
 
 
